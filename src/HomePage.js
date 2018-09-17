6d2e951480d9f9ec11ef143def4f1df54faa37dd
Sam Maggs <samuelcmaggs@gmail.com>
2018-09-17 14:09:10 +0100

    HomePage image added, buttons added for navigation

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
-import pizzaImage from './images/pizzaHomePage.jpg'
+import pizzaImage from "./images/pizzaHomePage.jpg";
+import Button from "./Button";
 
 class HomePage extends Component {
   constructor(props) {
     super(props);
   }
 
-
   render() {
-
-    
     return (
       <React.Fragment>
-          <img src={pizzaImage}></img>
-        
-        
+        <img src={pizzaImage} />
+        <Button buttonText={"Build"} />
+        <Button buttonText={"Random"} />
       </React.Fragment>
     );
   }
