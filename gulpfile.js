a2f851030a6412d8dfdb25fbf5ebdcbaa710acaf
Jen Hoskins <jen.c.hoskins@gmail.com>
2018-09-18 18:37:05 +0100

    Created working tasks in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+let watch = require('gulp-watch-sass');
+let rename = require('gulp-rename');
 
+gulp.task('default', defaultTask);
+
+function defaultTask(done) {
+
+  done();
+}
 
 gulp.task('sass', function () {
-    gulp.src('./src/styles/*.scss')
+    return gulp.src('./src/styles/scss/styles.scss')
         .pipe(sass())
-        .pipe(gulp.dest(function (f) {
-            return f.base;
-        }))
+        .pipe(gulp.dest('./src/styles/css/'))
+        .pipe(rename('styles.css'));
 });
 
-gulp.task('default', ['sass'], function () {
-    gulp.watch('./src/styles/*.scss', ['sass']);
-})
+gulp.task('watch', function () {
+    console.log("working");
+    gulp.watch('./src/styles/scss/styles.scss', ['sass']);
+});
