f9fc2bd8ca519194c014258c2b57c10e6a99d045
Andy Rankin <wave@andrewdrankin.com>
2018-09-18 13:24:08 +0100

    First steps of drop down options on the option page, placeholder images and colours

--- src/data/components/OptionsPage.js
+++ src/data/components/OptionsPage.js
@@ -3,32 +3,67 @@ import Button from "./Button";
 import { Link } from "react-router-dom";
 // import pizzaSauce from '../../images/pizza-sauce.jpg';
 import pizzaTopping from '../../images/pizza-toppings.jpg';
+import baseOptions from '../../images/base.jpg';
+import sauceOptions from '../../images/sauces.jpg';
+import toppingOptions from '../../images/ingredients.jpg';
+
 
 class OptionsPage extends Component {
   constructor(props) {
     super(props);
+    this.state ={
+      optionExpanded: false,
+    }
+    this.clickExpand = this.clickExpand.bind(this)
   }
-
+  clickExpand() {
+    this.setState ({
+      optionExpanded: !this.state.optionExpanded
+    })
+  }
   render() {
     return (
       <Fragment>
         <div>
-          <h1>
-            <Link to="/">Base</Link>
+          <h1 style={{
+            height: "150px",
+            background: "green"
+          }}>
+            <button onClick={ this.clickExpand }>Base</button>
+            <img style={{
+              width: "200px",
+              border: "2px solid #b0b0b0",
+              borderRadius: "100px",
+              float: "right"
+            }} src={baseOptions} />
           </h1>
+          { this.state.optionExpanded ? <p>list of options</p> : null }
         </div>
 
         <div>
-          <h1>
+          <h1 style={{ height: "150px" }}>
             <Link to="/">Sauce</Link>
-            {/* <img src={pizzaSauce} /> */}
+            <img style={{
+              width: "200px",
+              border: "2px solid #b0b0b0",
+              borderRadius: "100px",
+              float: "right"
+            }} src={sauceOptions} />
           </h1>
         </div>
 
         <div>
-          <h1>
+          <h1 style={{
+            height: "150px",
+            background: "red"
+          }}>
             <Link to="/">Toppings</Link>
-            <img src={pizzaTopping} />
+            <img style={{
+              width: "200px",
+              border: "2px solid #b0b0b0",
+              borderRadius: "100px",
+              float: "right"
+            }} src={toppingOptions} />
           </h1>
         </div>
         <Button buttonText={"Done"}/>
