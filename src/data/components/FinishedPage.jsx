a099e36a50ec51edca1e046d96959a7afee9bead
Jen Hoskins <jen.c.hoskins@gmail.com>
2018-09-21 11:06:13 +0100

    Added share button to FinishedPage

--- src/data/components/FinishedPage.jsx
+++ src/data/components/FinishedPage.jsx
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import Button from "./Button";
+
 
 class FinishedPage extends Component {
   constructor(props) {
     super(props);
+    this.handleShare = this.handleShare.bind(this);
+  }
+
+  handleShare() {
+
   }
 
   render() {
     const { data, chosenBase, chosenSauce, chosenToppings } = this.props;
     return (
-      <main className="container text-light my-3">
-        <h1 className="pizzagram-header-text">
-          Your Pizza
-        </h1>
-        <small className="text-primary">We're not judging...</small>
+      <main className="container text-light">
+        <header className="mb-4">
+          <h1 className="pizzagram-header-text">
+            Your Pizza
+          </h1>
+          <p className="lead">We're not judging...</p>
+        </header>
         <section className="my-3">
           <h2 className="pizzagram-header-text">Base</h2>
           { Object.keys(chosenBase).length ?
@@ -50,8 +59,13 @@ class FinishedPage extends Component {
             </ul> : <p className="text-primary">You haven't chosen any toppings for your pizza.</p>
           }
         </section>
-        <section className="my-3">
-          <Link to="/options" className="btn btn-primary">&lt; Back</Link>
+        <section className="button-container d-flex justify-content-between my-4 pb-5">
+          <div className="back-button-container w-25 mr-2">
+              <Link className="btn btn-primary btn-block" to="/options">&lt; Back</Link>
+          </div>
+          <div className="save-button-container w-75 ml-2">
+              <Button onClick={ this.handleShare } buttonText="Share Your Pizza" colourTheme="success" isBlock={true} />
+          </div>
         </section>
       </main>
     );
